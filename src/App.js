3a8bffd0ea012f1b79221cfc74632ff7dc0520db
Jiwon Lee <115334266+jiwonlee242@users.noreply.github.com>
2023-07-07 06:21:38 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Home from "./pages/home";
 import Post from "./pages/post";
 import QuizStart from "./pages/quizStart";
 import Quiz from "./pages/quiz";
+import QuizResult from "./pages/quizResult";
+import MyPage from "./pages/mypage";
+import Logout from "./pages/logout";
+import EditProfile from "./pages/editprofile";
+import DeleteAccount from "./pages/deleteaccount";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -18,6 +23,11 @@ function App() {
         <Route path="/post" element={<Post />} />
         <Route path="/quizstart" element={<QuizStart />} />
         <Route path="/quiz" element={<Quiz />} />
+        <Route path="/quizresult" element={<QuizResult />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/editprofile" element={<EditProfile />} />
+        <Route path="/deleteaccount" element={<DeleteAccount />} />
       </Routes>
     </>
   );
